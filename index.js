92d438746bb2cda1856e3a40b34708df11616ff9
santhu <santhuchary143@gmail.com>
2020-04-08 21:22:27 +0530

    milestone1

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 var Faker = require('./lib');
 var faker = new Faker({ locales: require('./lib/locales') });
 module['exports'] = faker;
+console.log(faker.fooditems.nonveg());
+console.log(faker.fooditems.veg());
+console.log(faker.fooditems.fruits());
+console.log(faker.fooditems.fastfood());
